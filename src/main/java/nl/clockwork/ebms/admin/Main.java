018929b1521dbac138f29547b8bbdb6e327a599a
eluinstra <edwin.luinstra@clockwork.nl>
2013-10-16 21:22:57 +0200

    created executable jar

---
+++ src/main/java/nl/clockwork/ebms/admin/Main.java
@@ -0,0 +1,34 @@
+package nl.clockwork.ebms.admin;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.FilterMapping;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.springframework.web.context.ContextLoaderListener;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		Server server = new Server(8080);
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+
+		context.setInitParameter("configuration","deployment");
+		context.setInitParameter("contextConfigLocation","classpath:applicationContext.xml");
+
+		context.addServlet(org.eclipse.jetty.servlet.DefaultServlet.class,"/");
+		
+		FilterHolder filterHolder = new FilterHolder(org.apache.wicket.protocol.http.WicketFilter.class); 
+		filterHolder.setInitParameter("applicationClassName","nl.clockwork.ebms.admin.web.WicketApplication");
+		filterHolder.setInitParameter("filterMappingUrlPattern","/*");
+		context.addFilter(filterHolder,"/*",FilterMapping.DEFAULT);
+		
+		ContextLoaderListener listener = new ContextLoaderListener();
+		context.addEventListener(listener);
+
+		server.start();
+		server.join();
+	}
+}
