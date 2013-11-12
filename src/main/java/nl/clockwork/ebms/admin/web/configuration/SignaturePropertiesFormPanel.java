616bc5879fbcdf8ff6f8b3113791063c57c11400
eluinstra <edwin.luinstra@clockwork.nl>
2013-11-12 22:32:13 +0100

    added signing selector

--- src/main/java/nl/clockwork/ebms/admin/web/configuration/SignaturePropertiesFormPanel.java
+++ src/main/java/nl/clockwork/ebms/admin/web/configuration/SignaturePropertiesFormPanel.java
@@ -4,10 +4,14 @@ import nl.clockwork.ebms.admin.web.configuration.JavaKeyStorePropertiesFormPanel
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.io.IClusterable;
 
@@ -30,15 +34,59 @@ public class SignaturePropertiesFormPanel extends Panel
 		{
 			super(id,new CompoundPropertyModel<SignaturePropertiesFormModel>(model));
 
-			add(new JavaKeyStorePropertiesFormPanel("properties",new PropertyModel<JavaKeyStorePropertiesFormModel>(getModelObject(),"keystoreProperties"),false));
+			CheckBox signing = new CheckBox("signing")
+			{
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public IModel<String> getLabel()
+				{
+					return Model.of(getLocalizer().getString("lbl.signing",SignaturePropertiesForm.this));
+				}
+			};
+			add(signing);
+
+			final JavaKeyStorePropertiesFormPanel keystoreProperties = new JavaKeyStorePropertiesFormPanel("keystoreProperties",new PropertyModel<JavaKeyStorePropertiesFormModel>(getModelObject(),"keystoreProperties"),false)
+			{
+				private static final long serialVersionUID = 1L;
+
+				public boolean isVisible()
+				{
+					return getModelObject().getSigning();
+				}
+			};
+			keystoreProperties.setOutputMarkupId(true);
+			add(keystoreProperties);
+
+			signing.add(new AjaxFormComponentUpdatingBehavior("onchange")
+      {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				protected void onUpdate(AjaxRequestTarget target)
+				{
+					//FIXME
+					//target.add(keystoreProperties);
+					target.add(SignaturePropertiesForm.this);
+				}
+      });
 		}
 	}
 
 	public static class SignaturePropertiesFormModel implements IClusterable
 	{
 		private static final long serialVersionUID = 1L;
+		private boolean signing = true;
 		private JavaKeyStorePropertiesFormModel keystoreProperties = new JavaKeyStorePropertiesFormModel();
 
+		public boolean getSigning()
+		{
+			return signing;
+		}
+		public void setSigning(boolean signing)
+		{
+			this.signing = signing;
+		}
 		public JavaKeyStorePropertiesFormModel getKeystoreProperties()
 		{
 			return keystoreProperties;
