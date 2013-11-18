3c110f8d207254d8a4f199a3b8a8636726ee15d0
eluinstra <edwin.luinstra@clockwork.nl>
2013-11-18 23:09:29 +0100

    fixed query overriden selectMessageTraffic

--- src/main/java/nl/clockwork/ebms/admin/dao/mssql/EbMSDAOImpl.java
+++ src/main/java/nl/clockwork/ebms/admin/dao/mssql/EbMSDAOImpl.java
@@ -15,12 +15,19 @@
  */
 package nl.clockwork.ebms.admin.dao.mssql;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
+import nl.clockwork.ebms.Constants.EbMSMessageStatus;
+import nl.clockwork.ebms.admin.Constants.TimeUnit;
 import nl.clockwork.ebms.admin.dao.AbstractEbMSDAO;
 import nl.clockwork.ebms.admin.web.message.EbMSMessageFilter;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.transaction.support.TransactionTemplate;
 
 public class EbMSDAOImpl extends AbstractEbMSDAO
@@ -38,10 +45,10 @@ public class EbMSDAOImpl extends AbstractEbMSDAO
 		//	" order by cpa_id" +
 		//	" offset " + first + " rows fetch " + count + " rows only"
 		//;
-		return "select * from (" +
+		return "select a.* from (" +
 			CPARowMapper.getBaseQuery().replaceFirst("select ","select row_number() over (order by cpa_id) as rn, ") +
-			//") where rn >= " + (first + 1) + " and rn < " + (first + 1 + count)
-			") where rn between " + first + " and " + (first + count)
+			//") a where rn >= " + (first + 1) + " and rn < " + (first + 1 + count)
+			") a where rn between " + first + " and " + (first + count)
 		;
 	}
 	
@@ -54,12 +61,60 @@ public class EbMSDAOImpl extends AbstractEbMSDAO
 		//	" order by time_stamp desc" +
 		//	" offset " + first + " rows fetch " + count + " rows only"
 		//;
-		return "select * from (" +
+		return "select a.* from (" +
 			new EbMSMessageRowMapper().getBaseQuery().replaceFirst("select ","select row_number() over (order by time_stamp desc) as rn, ") +
 			" where 1 = 1" +
 			getMessageFilter(filter,parameters) +
-			//") where rn >= " + (first + 1) + " and rn < " + (first + 1 + count)
-			") where rn between " + first + " and " + (first + count)
+			//") a where rn >= " + (first + 1) + " and rn < " + (first + 1 + count)
+			") a where rn between " + first + " and " + (first + count)
 		;
 	}
+
+	@Override
+	public HashMap<Date,Number> selectMessageTraffic(Date from, Date to, TimeUnit timeUnit, EbMSMessageStatus...status)
+	{
+		final HashMap<Date,Number> result = new HashMap<Date,Number>();
+		jdbcTemplate.query(
+			//"select format(time_stamp,'" + getDateFormat(timeUnit.getTimeUnitDateFormat()) + "') time, count(*) nr" + 
+			//"select parse(format(time_stamp,'" + getDateFormat(timeUnit.getTimeUnitDateFormat()) + "'),'yyyy-MM-dd hh:mm:ss') time, count(*) nr" + 
+			"select cast(" + getDateFormat(timeUnit.getTimeUnitDateFormat()) + "as datetime) time, count(*) nr" + 
+			" from ebms_message" + 
+			" where time_stamp >= ? " +
+			" and time_stamp < ?" +
+			(status.length == 0 ? " and status is not null" : " and status in (" + join(status,",") + ")") +
+			//" group by format(time_stamp,'" + getDateFormat(timeUnit.getTimeUnitDateFormat()) + "')",
+			" group by " + getDateFormat(timeUnit.getTimeUnitDateFormat()),
+			new ParameterizedRowMapper<Object>()
+			{
+				@Override
+				public Object mapRow(ResultSet rs, int rowNum) throws SQLException
+				{
+					result.put(rs.getTimestamp("time"),rs.getInt("nr"));
+					return null;
+				}
+			},
+			from,
+			to
+		);
+		return result;
+	}
+
+	protected String getDateFormat(String timeUnitDateFormat)
+	{
+		if ("mm".equals(timeUnitDateFormat))
+			//return "yyyy-MM-dd HH:mm:00";
+			return "cast(datepart(yyyy,time_stamp) as varchar(4)) + '-' + cast(datepart(mm,time_stamp) as varchar(2)) + '-' + cast(datepart(dd,time_stamp) as varchar(2)) + ' ' + cast(datepart(hh,time_stamp) as varchar(2)) + ':' + cast(datepart(mi,time_stamp) as varchar(2)) + ':' + '00'";
+		else if ("HH".equals(timeUnitDateFormat))
+			//return "yyyy-MM-dd HH:00:00";
+			return "cast(datepart(yyyy,time_stamp) as varchar(4)) + '-' + cast(datepart(mm,time_stamp) as varchar(2)) + '-' + cast(datepart(dd,time_stamp) as varchar(2)) + ' ' + cast(datepart(hh,time_stamp) as varchar(2)) + ':' + '00' + ':' + '00'";
+		else if ("dd".equals(timeUnitDateFormat))
+			//return "yyyy-MM-dd 00:00:00";
+			return "cast(datepart(yyyy,time_stamp) as varchar(4)) + '-' + cast(datepart(mm,time_stamp) as varchar(2)) + '-' + cast(datepart(dd,time_stamp) as varchar(2)) + ' ' + '00' + ':' + '00' + ':' + '00'";
+		else if ("MM".equals(timeUnitDateFormat))
+			//return "yyyy-MM-01 00:00:00";
+			return "cast(datepart(yyyy,time_stamp) as varchar(4)) + '-' + cast(datepart(mm,time_stamp) as varchar(2)) + '-' + '01' + ' ' + '00' + ':' + '00' + ':' + '00'";
+		else
+			return timeUnitDateFormat;
+	}
+
 }
