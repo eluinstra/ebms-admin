edf6dbd72a14c3f7808d593e3e1f206599d114ef
eluinstra <edwin.luinstra@clockwork.nl>
2014-12-27 21:23:41 +0100

    quickfix for random Connection reset exception with cxf client

--- src/main/java/nl/clockwork/ebms/admin/web/service/cpa/CPAUploadPage.java
+++ src/main/java/nl/clockwork/ebms/admin/web/service/cpa/CPAUploadPage.java
@@ -140,6 +140,9 @@ public class CPAUploadPage extends BasePage
 					catch (Exception e)
 					{
 						logger.error("",e);
+						//QuickFix
+						if ("Could not send Message.".equals(e.getMessage()))
+							setResponsePage(new CPAsPage());
 						error(e.getMessage());
 					}
 				}
