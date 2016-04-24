1bb39536a640c8902df0289efdba467043c930e3
eluinstra <e.luinstra@gmail.com>
2016-04-24 12:52:05 +0200

    cleanup

--- src/main/java/nl/clockwork/ebms/admin/web/service/message/PingPage.java
+++ src/main/java/nl/clockwork/ebms/admin/web/service/message/PingPage.java
@@ -39,7 +39,6 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.ResourceModel;
