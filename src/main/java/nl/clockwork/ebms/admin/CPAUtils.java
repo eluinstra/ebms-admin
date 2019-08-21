bda821276b6eb77fd37e34fcd5caf8434d6b6116
eluinstra <e.luinstra@gmail.com>
2019-08-21 21:58:53 +0200

    fixed partyId bug

--- src/main/java/nl/clockwork/ebms/admin/CPAUtils.java
+++ src/main/java/nl/clockwork/ebms/admin/CPAUtils.java
@@ -94,7 +94,7 @@ public class CPAUtils
 	{
 		LinkedHashSet<String> result = new LinkedHashSet<String>();
 		for (PartyInfo partyInfo : cpa.getPartyInfo())
-			if (partyId == null || !partyId.equals(partyInfo.getPartyId().get(0)))
+			if (partyId == null || !partyId.equals(toString(partyInfo.getPartyId().get(0))))
 				for (CollaborationRole role : partyInfo.getCollaborationRole())
 					if (roleName == null || !roleName.equals(role.getRole().getName()))
 						result.add(role.getRole().getName());
@@ -144,7 +144,7 @@ public class CPAUtils
 	{
 		ArrayList<CollaborationRole> result = new ArrayList<CollaborationRole>();
 		for (PartyInfo partyInfo : cpa.getPartyInfo())
-			if (partyId == null || !partyId.equals(partyInfo.getPartyId().get(0)))
+			if (partyId == null || !partyId.equals(toString(partyInfo.getPartyId().get(0))))
 				for (CollaborationRole role : partyInfo.getCollaborationRole())
 					if (role.getRole().getName().equals(roleName))
 						result.add(role);
