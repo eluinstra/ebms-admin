72e14333af2b71eeb183e4fecdc1b997967c3a7c
eluinstra <e.luinstra@gmail.com>
2019-09-11 20:09:27 +0200

    cleanup

--- src/main/java/nl/clockwork/ebms/admin/web/message/TrafficChartPage.java
+++ src/main/java/nl/clockwork/ebms/admin/web/message/TrafficChartPage.java
@@ -93,9 +93,7 @@ public class TrafficChartPage extends BasePage
 		result.setyAxis(new Axis().setTitle(new Title("Messages")));
 		result.setLegend(new Legend().setLayout(LegendLayout.VERTICAL).setAlign(HorizontalAlignment.RIGHT).setVerticalAlign(VerticalAlignment.TOP).setX(0).setY(1000).setBorderWidth(0));
 		result.setSeries(Arrays.stream(model.getEbMSMessageTrafficChartOption().getEbMSMessageTrafficChartSeries())
-				.map(s -> new SimpleSeries().setName(s.getName())
-						.setColor(s.getColor())
-						.setData(getMessages(dates,model,s.getEbMSMessageStatuses())))
+				.map(s -> new SimpleSeries().setName(s.getName()).setColor(s.getColor()).setData(getMessages(dates,model,s.getEbMSMessageStatuses())))
 				.collect(Collectors.toList()));
 		return result;
 	}
