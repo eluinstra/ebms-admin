fdb16a787d30a2c109208f039bca5803187fad84
eluinstra <eluinstra@luin.dev>
2020-08-11 19:06:13 +0200

    added support for environment variables using _ instead of .

--- src/main/java/nl/clockwork/ebms/admin/PropertySourcesPlaceholderConfigurer.java
+++ src/main/java/nl/clockwork/ebms/admin/PropertySourcesPlaceholderConfigurer.java
@@ -52,6 +52,7 @@ public class PropertySourcesPlaceholderConfigurer extends org.springframework.co
 		result.putAll(properties.entrySet().stream()
 				.map(e -> System.getProperty((String)e.getKey()) == null ? e : new AbstractMap.SimpleEntry<String,String>((String)e.getKey(),System.getProperty((String)e.getKey())))
 				.map(e -> System.getenv((String)e.getKey()) == null ? e : new AbstractMap.SimpleEntry<String,String>((String)e.getKey(),System.getenv((String)e.getKey())))
+				.map(e -> System.getenv(((String)e.getKey()).replaceAll("\\.","_")) == null ? e : new AbstractMap.SimpleEntry<String,String>((String)e.getKey(),System.getenv(((String)e.getKey()).replaceAll("\\.","_"))))
 				.collect(Collectors.toMap(e -> (String)e.getKey(), e -> (String)e.getValue())));
 		return result;
 	}
