36c1d9de17814af94f9b1c97402cc4e64abf56c4
John Wentink <john.wentink@gmail.com>
2021-04-22 14:29:44 +0200

    remove property-dump

--- src/main/java/nl/clockwork/ebms/admin/PropertySourcesPlaceholderConfigurer.java
+++ src/main/java/nl/clockwork/ebms/admin/PropertySourcesPlaceholderConfigurer.java
@@ -55,7 +55,6 @@ public class PropertySourcesPlaceholderConfigurer extends org.springframework.co
 				.map(e -> System.getenv(((String)e.getKey()).replaceAll("\\.","_")) == null ? e : new AbstractMap.SimpleEntry<String,String>((String)e.getKey(),System.getenv(((String)e.getKey()).replaceAll("\\.","_"))))
 				.collect(Collectors.toMap(e -> (String)e.getKey(), e -> (String)e.getValue())));
 		
-		result.store(System.out, "DUMP");
 		return result;
 	}
 }
