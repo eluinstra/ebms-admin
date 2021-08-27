42cff4c25ead1a19b165684c7bb501c02ad07ac5
eluinstra <eluinstra@luin.dev>
2021-08-27 22:25:05 +0200

    fixed issues

--- src/main/java/nl/clockwork/ebms/admin/web/EmbeddedWebConfig.java
+++ src/main/java/nl/clockwork/ebms/admin/web/EmbeddedWebConfig.java
@@ -1,15 +1,15 @@
 /**
  * Copyright 2013 Clockwork
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License,Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing,software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -24,8 +24,10 @@ import javax.xml.ws.Endpoint;
 import javax.xml.ws.soap.SOAPBinding;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 import org.apache.cxf.binding.BindingFactoryManager;
@@ -133,7 +135,7 @@ public class EmbeddedWebConfig
 		return result;
 	}
 
-	private Endpoint publishEndpoint(Object service, String address, String namespaceUri, String serviceName, String endpointName)
+	private Endpoint publishEndpoint(Object service,String address,String namespaceUri,String serviceName,String endpointName)
 	{
 		val result = new EndpointImpl(cxf(),service);
 		result.setAddress(address);
@@ -152,7 +154,7 @@ public class EmbeddedWebConfig
 		sf.setProvider(createJacksonJsonProvider());
 		sf.setFeatures(Arrays.asList(new OpenApiFeature()));
 		sf.setResourceClasses(getResourceClasses().keySet().toJavaList());
-		getResourceClasses().forEach((resourceClass,resourceObject) -> createResourceProvider(sf, resourceClass, resourceObject));
+		getResourceClasses().forEach((resourceClass,resourceObject) -> createResourceProvider(sf,resourceClass,resourceObject));
 		val manager = sf.getBus().getExtension(BindingFactoryManager.class);
 		val factory = new JAXRSBindingFactory();
 		factory.setBus(sf.getBus());
@@ -162,12 +164,11 @@ public class EmbeddedWebConfig
 
 	Map<Class<?>,Object> getResourceClasses()
 	{
-		val result = HashMap.<Class<?>,Object>ofEntries(
-			Tuple.of(CPAServiceImpl.class, cpaService),
-			Tuple.of(URLMappingServiceImpl.class, urlMappingService),
-			Tuple.of(CertificateMappingServiceImpl.class, certificateMappingService),
-			Tuple.of(EbMSMessageServiceImpl.class, ebMSMessageService));
-		return result;
+		return HashMap.<Class<?>,Object>ofEntries(
+			Tuple.of(CPAServiceImpl.class,cpaService),
+			Tuple.of(URLMappingServiceImpl.class,urlMappingService),
+			Tuple.of(CertificateMappingServiceImpl.class,certificateMappingService),
+			Tuple.of(EbMSMessageServiceImpl.class,ebMSMessageService));
 	}
 
 	private JacksonJsonProvider createJacksonJsonProvider()
@@ -180,18 +181,20 @@ public class EmbeddedWebConfig
 	private ObjectMapper createObjectMapper() {
 		val result = new ObjectMapper();
 		result.registerModule(new Jdk8Module());
+		result.registerModule(new JavaTimeModule());
 		result.registerModule(createSimpleModule());
+		result.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
 		return result;
 	}
 
 	private SimpleModule createSimpleModule() {
 		val result = new SimpleModule();
-		result.addSerializer(X509Certificate.class, new X509CertificateSerializer());
-		result.addDeserializer(X509Certificate.class, new X509CertificateDeserializer());
+		result.addSerializer(X509Certificate.class,new X509CertificateSerializer());
+		result.addDeserializer(X509Certificate.class,new X509CertificateDeserializer());
 		return result;
 	}
 
-	private void createResourceProvider(JAXRSServerFactoryBean sf, Class<?> resourceClass, Object resourceObject) {
-		sf.setResourceProvider(resourceClass, new SingletonResourceProvider(resourceObject));
+	private void createResourceProvider(JAXRSServerFactoryBean sf,Class<?> resourceClass,Object resourceObject) {
+		sf.setResourceProvider(resourceClass,new SingletonResourceProvider(resourceObject));
 	}
 }
