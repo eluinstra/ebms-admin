4fea554944dbd70b6667a5fc5ff218d5ba8d0cb7
eluinstra <eluinstra@luin.dev>
2024-09-06 00:53:42 +0200

    fixed issues

--- src/main/java/nl/clockwork/ebms/admin/EmbeddedAppConfig.java
+++ src/main/java/nl/clockwork/ebms/admin/EmbeddedAppConfig.java
@@ -15,17 +15,49 @@
  */
 package nl.clockwork.ebms.admin;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.val;
+import nl.clockwork.ebms.CommonConfig;
+import nl.clockwork.ebms.admin.dao.AdminDAOConfig;
+import nl.clockwork.ebms.admin.web.EmbeddedWebConfig;
+import nl.clockwork.ebms.cache.CacheConfig;
+import nl.clockwork.ebms.cpa.CPAManagerConfig;
+import nl.clockwork.ebms.cpa.CPAServiceConfig;
+import nl.clockwork.ebms.cpa.certificate.CertificateMappingServiceConfig;
+import nl.clockwork.ebms.cpa.url.URLMappingServiceConfig;
+import nl.clockwork.ebms.dao.DAOConfig;
+import nl.clockwork.ebms.datasource.DataSourceConfig;
+import nl.clockwork.ebms.delivery.DeliveryManagerConfig;
+import nl.clockwork.ebms.delivery.client.EbMSClientConfig;
+import nl.clockwork.ebms.delivery.task.DeliveryTaskHandlerConfig;
+import nl.clockwork.ebms.delivery.task.DeliveryTaskManagerConfig;
+import nl.clockwork.ebms.encryption.EncryptionConfig;
+import nl.clockwork.ebms.event.MessageEventListenerConfig;
+import nl.clockwork.ebms.jms.JMSConfig;
+import nl.clockwork.ebms.kafka.KafkaConfig;
+import nl.clockwork.ebms.processor.EbMSProcessorConfig;
+import nl.clockwork.ebms.querydsl.model.QueryDSLConfig;
+import nl.clockwork.ebms.scheduler.SchedulerConfig;
+import nl.clockwork.ebms.security.KeyStoreConfig;
+import nl.clockwork.ebms.server.EbMSServerConfig;
+import nl.clockwork.ebms.service.EbMSMessageServiceConfig;
+import nl.clockwork.ebms.signing.SigningConfig;
+import nl.clockwork.ebms.transaction.TransactionManagerConfig;
+import nl.clockwork.ebms.validation.ValidationConfig;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
-import lombok.AccessLevel;
-import lombok.val;
-import lombok.experimental.FieldDefaults;
-
 @Configuration
+@Import({AdminDAOConfig.class, CertificateMappingServiceConfig.class, CacheConfig.class, CommonConfig.class, CPAManagerConfig.class, CPAServiceConfig.class,
+		DAOConfig.class, DataSourceConfig.class, DeliveryManagerConfig.class, DeliveryTaskManagerConfig.class, DeliveryTaskHandlerConfig.class, EbMSClientConfig.class,
+		EbMSMessageServiceConfig.class, EbMSProcessorConfig.class, EbMSServerConfig.class, EmbeddedWebConfig.class, EncryptionConfig.class, JMSConfig.class,
+		KafkaConfig.class, KeyStoreConfig.class, nl.clockwork.ebms.security.azure.KeyStoreConfig.class, MessageEventListenerConfig.class, QueryDSLConfig.class,
+		SchedulerConfig.class, SigningConfig.class, TransactionManagerConfig.class, URLMappingServiceConfig.class, ValidationConfig.class})
 @PropertySource(
 		value = {"classpath:nl/clockwork/ebms/default.properties", "classpath:nl/clockwork/ebms/admin/default.properties",
 				"classpath:nl/clockwork/ebms/cache/ignite/default.properties", "file:${ebms.configDir}ebms-admin.embedded.advanced.properties",
@@ -34,7 +66,12 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EmbeddedAppConfig
 {
-	public static PropertySourcesPlaceholderConfigurer PROPERTY_SOURCE = propertySourcesPlaceholderConfigurer();
+	public static final PropertySourcesPlaceholderConfigurer PROPERTY_SOURCE = propertySourcesPlaceholderConfigurer();
+
+	public EmbeddedAppConfig()
+	{
+		//do nothing
+	}
 
 	private static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer()
 	{
